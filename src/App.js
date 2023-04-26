c79502ea1230d650df73ca016d78951844f58601
zaynmuhammad <zaynmuhammad4@hotmail.com>
2023-04-26 16:40:54 -0400

    Added functions for POST and GET to save and retrieve a character.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from './consts.js';
+import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST, API_URL } from './consts.js';
 import { Attributes, Classes, Skills } from './components';
 
 
@@ -11,6 +11,31 @@ function App() {
   const [intelligence, setIntelligence] = useState(7);
   const [wisdom, setWisdom] = useState(7);
   const [charisma, setCharisma] = useState(7);
+
+  async function handlePost(e) {
+    e.preventDefault();
+    fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        strength,
+        dexterity,
+        constitution,
+        intelligence,
+        wisdom,
+        charisma
+      })
+    })
+  }
+
+  async function handleGet(e) {
+    e.preventDefault();
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    console.log(data);
+  }
 
   return (
     <div className="App">
@@ -52,6 +77,14 @@ function App() {
             charisma={charisma}
           />
         </div>
+        {/* TODO: put this in a seperate component */}
+        <form onSubmit={handlePost}>
+          <button type='submit'>Save Character</button>
+
+        </form>
+        <form onSubmit={handleGet}>
+          <button type='submit'>Get Character</button>
+        </form>
       </section>
     </div>
   );
